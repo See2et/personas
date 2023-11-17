c4bfc77f341c8a686b3428d0de2638f2f0dadbaa
see2et <seezet1024@gmail.com>
2023-11-17 21:02:27 +0900

    smarter

--- src/sendWebhook.ts
+++ src/sendWebhook.ts
@@ -1,34 +1,16 @@
 import config from "../config.json" assert { type: "json" };
+import personas from "../personas.json" assert { type: "json" };
 
 export function sendWebhook(id: string, content: string) {
-  let username: string;
-  let avatar_url: string | null = null;
-  switch (id) {
-    case "se":
-      username = "See2et / しーぜっと";
-      avatar_url =
-        "https://pub-33506eaed9964123b5ace46f04f3d73a.r2.dev/files/04e2c480-e834-4567-8d1e-b12fd698aa0c.webp";
-      break;
-    case "sh":
-      username = "sh1rase / 白瀬ぬい";
-      avatar_url =
-        "https://pub-33506eaed9964123b5ace46f04f3d73a.r2.dev/files/81d7f946-8fd6-4327-8ae8-e0ab4ce0f993.webp";
-      break;
-    case "k0":
-      username = "k0phy / こふぃー";
-      break;
-    case "m1":
-      username = "m1nadzuki / 水無月むな";
-      break;
-    default:
-      username = "unknown";
-      break;
-  }
+  const info = personas.find((v) => {
+    if (!v.id) return false;
+    if (v.id !== id) return false;
+    return true;
+  }) ?? { id: "", username: "unknown" };
 
   const body = JSON.stringify({
-    username,
     content,
-    avatar_url,
+    ...info,
   });
 
   fetch(
