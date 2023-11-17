deeff4aeff76289710e03f696335efdc4be4490a
see2et <seezet1024@gmail.com>
2023-11-17 16:13:14 +0900

    load config

--- main.ts
+++ main.ts
@@ -1,8 +1,2 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import config from "./config.json" assert { type: "json" };
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
